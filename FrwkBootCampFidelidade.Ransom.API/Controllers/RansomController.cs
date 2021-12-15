2af2e96e67f7b64cee72bad4b491f17ea333b5a1
Gerlan de Paula <jgerlan@gmail.com>
2021-12-15 16:35:17 -0300

    Adiciona projeto com controller e definições da API de Resgate.

---
+++ FrwkBootCampFidelidade.Ransom.API/Controllers/RansomController.cs
@@ -0,0 +1,33 @@
+﻿using FrwkBootCampFidelidade.Aplicacao.Interfaces;
+using FrwkBootCampFidelidade.DTO.RansomContext;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FrwkBootCampFidelidade.Ransom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RansomController : ControllerBase
+    {
+        private readonly IRansomService _ransomService;
+        public RansomController(IRansomService ransomService)
+        {
+            _ransomService = ransomService;
+        }
+
+        [HttpGet]
+        public IEnumerable<RansomDTO> GetAll()
+        {
+            IEnumerable<RansomDTO> ransoms = _ransomService.GetAll();
+            return ransoms;
+        }
+
+        [HttpGet("GetByCPF/{cpf}")]
+        public async Task<IEnumerable<RansomDTO>> GetByCPF(string cpf)
+        {
+            IEnumerable<RansomDTO> bonifications = await _ransomService.GetListByCPF(cpf);
+            return bonifications;
+        }
+    }
+}
