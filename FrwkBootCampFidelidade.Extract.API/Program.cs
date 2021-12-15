2056be0658e4502094a7e346aed72ea53de68940
Giovanni Christian Debona <37405805+giovannidebona@users.noreply.github.com>
2021-12-15 15:27:05 -0300

    finalAPI

--- FrwkBootCampFidelidade.Extract.API/Program.cs
+++ FrwkBootCampFidelidade.Extract.API/Program.cs
@@ -1,11 +1,6 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FrwkBootCampFidelidade.Extract.API
 {
@@ -15,9 +10,9 @@ namespace FrwkBootCampFidelidade.Extract.API
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
