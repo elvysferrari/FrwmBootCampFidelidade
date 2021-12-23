b35f6427b8bfb2c6b65ac4bafac059539f10f4cd
Elvys Ferrari <elvys.ferrari@gmail.com>
2021-12-23 10:23:00 -0300

    docker-compose

---
+++ FrwkBootCampFidelidade.Wallet.API/HostedServices/MigrationHostedService.cs
@@ -0,0 +1,32 @@
+﻿using FrwkBootCampFidelidade.Infraestrutura.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrwkBootCampFidelidade.Wallet.API.HostedServices
+{
+    public class MigrationHostedService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public MigrationHostedService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            using var ctx = scope.ServiceProvider.GetRequiredService<DBContext>();
+            await ctx.Database.MigrateAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- FrwkBootCampFidelidade.Wallet.API/Startup.cs
+++ FrwkBootCampFidelidade.Wallet.API/Startup.cs
@@ -2,6 +2,7 @@ using Autofac;
 using FluentValidation.AspNetCore;
 using FrwkBootCampFidelidade.Infraestrutura.Context;
 using FrwkBootCampFidelidade.Infraestrutura.IOC.IOC;
+using FrwkBootCampFidelidade.Wallet.API.HostedServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace FrwkBootCampFidelidade.Wallet.API
 {
@@ -24,6 +26,11 @@ namespace FrwkBootCampFidelidade.Wallet.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Configuration.GetValue<bool>("RunMigration"))
+            {
+                services.AddHostedService<MigrationHostedService>();
+            }
+
             services.AddDbContext<DBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("connection")));
 
             services.AddDBInjector();
@@ -44,7 +51,7 @@ namespace FrwkBootCampFidelidade.Wallet.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
+
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FrwkBootCampFidelidade.Wallet.API v1"));            
