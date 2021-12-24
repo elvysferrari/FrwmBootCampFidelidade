505a29cf037bb6dd250d0ea02bb7d5af9cd5224b
Fernando Nogueira <fgeraldo11@gmail.com>
2021-12-24 15:09:59 -0300

    Adicionado o projeto Core 'compartilhado'

---
+++ FrwkBootCampFidelidade.Core/Constants/DomainConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace FrwkBootCampFidelidade.Core.Constants
+{
+    public static class DomainConstant
+    {
+        public const string QUEUE_PROMOTIONS = "PROMOTIONS";
+        public const string QUEUE_PRODUCTS = "PRODUCTS";
+    }
+}
---
+++ FrwkBootCampFidelidade.Core/Constants/MethodConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace FrwkBootCampFidelidade.Core.Constants
+{
+    public class MethodConstant
+    {
+        public const string GET = "GET";
+    }
+}
