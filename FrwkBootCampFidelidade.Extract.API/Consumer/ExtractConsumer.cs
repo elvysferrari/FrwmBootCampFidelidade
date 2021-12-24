917d82b89c307b51e526b09e9553ac91d0f7322b
jpcysne <joaorubio@frwk.com.br>
2021-12-24 16:59:29 -0300

    Criação da ExtractConsumer

---
+++ FrwkBootCampFidelidade.Extract.API/Consumer/ExtractConsumer.cs
@@ -0,0 +1,22 @@
+﻿using FrwkBootCampFidelidade.Extract.API.Options;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrwkBootCampFidelidade.Extract.API.Consumer
+{
+    public class ExtractConsumer : BackgroundService
+    {
+        private readonly ExtractRabbitMqConfiguration _config;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IServiceProvider _serviceProvider;
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FrwkBootCampFidelidade.Extract.API/Options/ExtractRabbitMqConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace FrwkBootCampFidelidade.Extract.API.Options
+{
+    public class ExtractRabbitMqConfiguration
+    {
+        public string Host { get; set; }
+        //public string Username { get; set; }
+        //public string Password { get; set; }
+    }
+}
