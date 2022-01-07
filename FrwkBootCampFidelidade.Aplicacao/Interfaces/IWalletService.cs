e6b51a20a026bc5d3a1e0cbda045d44ae7e5c412
Rodrigo Cardoso <rodrigo_cardos97@hotmail.com>
2022-01-07 16:16:58 -0300

    update wallet retornando uma task

--- FrwkBootCampFidelidade.Aplicacao/Interfaces/IWalletService.cs
+++ FrwkBootCampFidelidade.Aplicacao/Interfaces/IWalletService.cs
@@ -7,7 +7,7 @@ namespace FrwkBootCampFidelidade.Aplicacao.Interfaces
     public interface IWalletService
     {
         Task Add(WalletDTO wallet);
-        void Update(WalletDTO wallet);
+        Task Update(WalletDTO wallet);
         Task<List<WalletDTO>> GetAllByUserId(int userId);
         Task<List<WalletDTO>> GetByUserIdAndType(int userId, int walletType);
         Task Transfer(WalletTransferDTO walletTransferDTO);
