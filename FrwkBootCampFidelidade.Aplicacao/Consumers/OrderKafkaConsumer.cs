f2eec65722b44493752a76f3a5962f1d0647ddea
fleal07 <fleal07@gmail.com>
2022-01-14 12:23:46 -0300

    feat:Implementacao Consumer Kafka

---
+++ FrwkBootCampFidelidade.Aplicacao/Consumers/OrderKafkaConsumer.cs
@@ -0,0 +1,122 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FrwkBootCampFidelidade.Aplicacao.Consumers
+{
+    public class OrderKafkaConsumer : BackgroundService
+    {
+        private readonly string nomeTopic = "Orders";
+
+        public OrderKafkaConsumer()
+        {
+            //var config = new ConsumerConfig
+            //{
+            //    BootstrapServers = "localhost:29092",
+            //    GroupId = $"Orders-group-0",
+            //    AutoOffsetReset = AutoOffsetReset.Earliest
+            //};
+
+
+            //CancellationTokenSource cts = new CancellationTokenSource();
+            //Console.CancelKeyPress += (_, e) =>
+            //{
+            //    e.Cancel = true;
+            //    cts.Cancel();
+            //};
+
+            //try
+            //{
+            //    using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            //    {
+            //        consumer.Subscribe(nomeTopic);
+
+            //        try
+            //        {
+            //            while (true)
+            //            {
+            //                var cr = consumer.Consume(cts.Token);
+            //            }
+            //        }
+            //        catch (OperationCanceledException)
+            //        {
+            //            consumer.Close();
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Exceção: {ex.GetType().FullName} | " +
+            //                 $"Mensagem: {ex.Message}");
+            //}
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = $"Orders-group-0",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe(nomeTopic);
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            consumer.Commit(cr);
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumer.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exceção: {ex.GetType().FullName} | " +
+                             $"Mensagem: {ex.Message}");
+            }
+
+            return Task.CompletedTask;
+        }
+
+            //    return Task.CompletedTask;
+            //}
+
+            //private async Task<string> InvokeService(MessageInputModel message)
+            //{
+            //    using var scope = _serviceProvider.CreateScope();
+            //    var OrderService = scope.ServiceProvider.GetRequiredService<IOrderService>();
+
+            //    dynamic response = string.Empty;
+
+            //    switch (message.Method)
+            //    {
+            //        case MethodConstant.POST:
+            //            response = await OrderService.Add(JsonConvert.DeserializeObject<OrderDTO>(message.Content));
+            //            break;
+            //    }
+
+            //    return JsonConvert.SerializeObject(response);
+            //}
+        }
+}
---
+++ Web.BootCampFidelidade.HttpAggregator/Controller/OrderKafkaController.cs
@@ -0,0 +1,75 @@
+﻿using FrwkBootCampFidelidade.Aplicacao.Constants;
+using FrwkBootCampFidelidade.Aplicacao.Interfaces.RpcService;
+using FrwkBootCampFidelidade.Dominio.Base;
+using Web.BootCampFidelidade.HttpAggregator.Models.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using System;
+
+namespace Web.BootCampFidelidade.HttpAggregator.Controller
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderKafkaController : ControllerBase
+    {
+        private readonly IRpcClientService service;
+        private readonly string bootstrapServers = "localhost:29092";
+        private readonly string nomeTopic = "Orders";
+
+        public OrderKafkaController(IRpcClientService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(OrderDTO), StatusCodes.Status201Created)]
+        public async Task<IActionResult> AddOrder([FromBody][Required] OrderDTO orderDTO)
+        {
+            var message = new MessageInputModel(DomainConstant.ORDER, MethodConstant.POST, JsonConvert.SerializeObject(orderDTO));
+
+            var response = await service.Call(message);
+            service.Close();
+
+            if (response.Equals(""))
+                return NotFound();
+
+            var orders = JsonConvert.DeserializeObject<OrderDTO>(response);
+
+            return Created($"{Request.Path}/{orders.Id}", orders);
+        }
+
+        public async Task<IActionResult> AddOrderKafka([FromBody][Required] string order) 
+        {
+            try
+            {
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServers
+                };
+
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    var result = await producer.ProduceAsync(
+                        nomeTopic,
+                        new Message<Null, string>
+                        { Value = nomeTopic });
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Created($"{Request.Path}", order);
+        }
+
+    }
+}
